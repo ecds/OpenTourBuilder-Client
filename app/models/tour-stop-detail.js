0d2bba1e6dd9cb1b8e6539fe793f02cd624d949b
Jay Varner <jayvarner@gmail.com>
2015-04-20 12:02:09 -0400

    Adding more styles.

--- app/models/tour-stop-detail.js
+++ app/models/tour-stop-detail.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
  	name: DS.attr('string'),
+ 	slug: DS.attr('string'),
 	tour: DS.belongsTo('tourDetail'),
 	map_image: DS.attr('string'),
 	position: DS.attr('number'),
