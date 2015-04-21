f5f9ec480b86b9b794c116fe578461cbbc5a3ac0
Jay Varner <jayvarner@gmail.com>
2015-04-20 21:40:11 -0400

    Working on maps and more styling. [#92708330]

---
+++ app/components/overview-map.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+	didInsertElement: function() {
+
+		this.sendAction();
+	}
+
+
+});
--- app/routes/tour.js
+++ app/routes/tour.js
@@ -7,4 +7,71 @@ export default Ember.Route.extend({
 		return this.store.find('tourDetail', params.slug);
 	},
 
+	didInsertElement: function() {
+
+	},
+
+	actions: {
+		mapTour: function initializeMap(){
+
+			$(document).ready(function(){
+  $(".dropdown-button").click(function() {
+    $(".dropdown-menu").toggleClass("show-menu");
+    $(".dropdown-menu > li").click(function(){
+      $(".dropdown-menu").removeClass("show-menu");
+    });
+    $(".dropdown-menu.dropdown-select > li").click(function() {
+      $(".dropdown-button").html($(this).html());
+    });
+  });
+});
+
+
+
+			var tour = DS.PromiseObject.create({
+				promise: this.store.find('tourDetail', this.currentModel.id)
+			});
+
+			tour.then(function(){
+				var stops = tour.get('content.stop_ids').get('content.currentState');
+
+				$.each(stops, function(index, value){
+				
+					var stop = DS.PromiseObject.create({
+						promise: this.store.find('tourStopDetail', value.id)
+					});
+
+					stop.then(function(){
+
+						if (stop.get('content.intro')===false) {
+							var stopCords = new google.maps.LatLng(
+					      		stop.get('content.lat'),
+					      		stop.get('content.lng')
+		      				);
+
+		      				var marker = new google.maps.Marker({
+							      position: stopCords,
+							      map: map,
+							      bounds: true
+							 });
+		      			}
+					});
+
+				});
+			});
+
+			var container = $(".overview-map");
+
+			var options = {
+      			center: new google.maps.LatLng(33.754318, -84.389791),
+      			zoom: 12,
+      			mapTypeId: google.maps.MapTypeId.ROADMAP
+    		};
+
+			var map = new google.maps.Map(container[0], options);
+
+		}
+
+	}
+
 });
