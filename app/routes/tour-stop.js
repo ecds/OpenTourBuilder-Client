03f8a485c378947c2e76474592ac402f4d8960fa
Jay Varner <jayvarner@gmail.com>
2015-04-28 13:38:29 -0400

    Yet more CSS and mapping working better.

--- app/routes/tour-stop.js
+++ app/routes/tour-stop.js
@@ -5,4 +5,126 @@ export default Ember.Route.extend({
 		return this.store.find('tourStopDetail', params.id);
 	},
 
+	actions: {
+		showDirections: function(lat, lng){
+			console.log(lng);
+			
+			Ember.$(".stop-article").toggle();
+			Ember.$(".stop-directions").toggle();
+			Ember.$("#directions").empty();
+
+			var selectedMode = document.getElementById('mode_select').value;
+
+			Ember.$(document).on('change','#mode_select',function(){
+				Ember.$("#directions").empty();
+				var selectedMode = document.getElementById('mode_select').value;
+				initializeMap(selectedMode, lat, lng);
+
+			});
+
+			function initializeMap(selectedMode, lat, lng){
+
+				console.log(selectedMode);
+				console.log(lat);
+
+				Ember.$(".loading").show();
+
+				var container = Ember.$(".map-canvas");
+
+		    	var stop = new google.maps.LatLng(
+		      		lat,
+		      		lng
+		      	);
+
+		    	navigator.geolocation.getCurrentPosition(
+		    		successCallback,
+	                errorCallback,{
+	                	maximumAge: 0,
+	        			timeout:50000
+	        		}
+	        	);
+
+	    		function successCallback(position) {}
+
+	    		function errorCallback() {
+	      			Ember.$(".loading").hide();
+			    	var marker = new google.maps.Marker({
+			    		position: stop,
+			    		map: map,
+			    	});
+			    	marker.setMap(map);
+	    		}
+
+				navigator.geolocation.getCurrentPosition(function(position){
+
+		    		var currentLatLng = new google.maps.LatLng(
+		  				position.coords.latitude,
+		  				position.coords.longitude
+		  			);
+
+					var request = {
+					    origin:currentLatLng,
+					    destination:stop,
+						travelMode: google.maps.TravelMode[selectedMode]
+					};
+
+					var directionsService = new google.maps.DirectionsService();
+					var directionsDisplay = new google.maps.DirectionsRenderer();
+					 
+					directionsService.route(request, function(result, status) {
+						if (status === google.maps.DirectionsStatus.OK) {
+
+							directionsDisplay.setMap(map);
+							
+							// Draw route to map
+					    	directionsDisplay.setDirections(result);
+
+					    	// Set the value the user last used
+					    	Ember.$("#mode_select").val(selectedMode);
+
+					    	// List turn-by-turn directions
+						    directionsDisplay.setPanel(document.getElementById('directions'));
+
+						    // Hide the loading indicator.
+						    Ember.$(".loading").hide();
+
+						    // Show mode selector.
+						    Ember.$("#mode_select").show();
+
+						    //initializeMap(selectedMode, lat, lng);
+
+
+					    }
+
+					    else {console.log('dang');}
+					});
+		    	});
+
+				var options = {
+	      			center: stop,
+	      			zoom: 17,
+	      			mapTypeId: google.maps.MapTypeId.ROADMAP
+	    		};
+
+
+				var map = new google.maps.Map(container[0], options);
+
+			}
+
+			//Ember.run.later((function() {
+
+
+			initializeMap(selectedMode, lat, lng);
+
+			//}), 5000);
+
+	  	},
+		
+		hideMap: function() {
+			Ember.$(".stop-article").show();
+			Ember.$(".stop-directions").hide();
+		}
+	}
+	
+
 });
